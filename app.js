fc7e38750cae29ab5cc37e4f678e136fc71e9481
Miller2406 <140726735+Miller2406@users.noreply.github.com>
2023-11-13 14:20:29 +0700

    update

---
+++ app.js
@@ -0,0 +1,9 @@
+const objA = {
+    name: 'Peter',
+    age: 24,
+    job: 'programmer',
+    city: 'BKK'
+}
+console.log(objA);
+let objALength = Object.keys(objA).length;
+console.log(objALength);
